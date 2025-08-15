eb92aacc3d4af0718e6b3247aeb977a3878893d4
ArtiyxDev <220812622+ArtiyxDev@users.noreply.github.com>
2025-08-14 22:41:47 -0500

    added little blur

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
    return (
       <footer>
-         <div className="mx-auto w-full border-t border-purple-600 py-4 text-center dark:border-gray-700">
+         <div className="mx-auto w-full border-t border-purple-600 py-4 text-center backdrop-blur-[2px] dark:border-gray-700">
             <p className="text-sm text-gray-500">
                &copy; {new Date().getFullYear()} {t('footer.copyright')}
             </p>
